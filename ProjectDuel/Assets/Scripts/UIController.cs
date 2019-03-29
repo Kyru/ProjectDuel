8c52cdcfd6d9faf1a86b81bf844a01e6b9e88b41
Álvaro Grafiá <avox97@gmail.com>
2019-03-29 12:24:21 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ ProjectDuel/Assets/Scripts/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class UIController: MonoBehaviour
+{
+	[SerializeField] private Text HP_Label_Blue;
+	[SerializeField] private GameObject blue_crab;
+	
+	void Update() 
+	{
+		HP_Label_Blue.text= blue_crab.GetComponent<PlayerCharacter>().get_health().ToString();
+	}
+}
