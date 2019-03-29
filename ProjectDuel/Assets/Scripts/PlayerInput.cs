598d159ccd6e6f88a6b2e0d8325a727e0cbc920f
Radosvet <radosv1g@gmail.com>
2019-03-29 13:38:27 +0100

    Scripts y prefabs mejorados

---
+++ ProjectDuel/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    [SerializeField] private KeyCode keyMovement1;
+    [SerializeField] private KeyCode keyMovement2;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKey(keyMovement1))
+        {
+            Vector3 movement = new Vector3(1 * speed, 0, 0);
+            //movement = transform.TransformDirection(movement);
+            transform.Translate(movement * Time.deltaTime);
+        }
+        if (Input.GetKey(keyMovement2))
+        {
+            Vector3 movement = new Vector3(-1 * speed, 0, 0);
+            //movement = transform.TransformDirection(movement);
+            transform.Translate(movement * Time.deltaTime);
+        }
+    }
+}
+
--- ProjectDuel/Assets/Scripts/Shoot.cs
+++ ProjectDuel/Assets/Scripts/Shoot.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShootLeft : MonoBehaviour
+public class Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject ballPrefab;
     private GameObject _ball;
+    [SerializeField] private KeyCode keyShoot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown("d"))
+        if (Input.GetKeyDown(keyShoot))
         {
             _ball = Instantiate(ballPrefab) as GameObject;
             _ball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
