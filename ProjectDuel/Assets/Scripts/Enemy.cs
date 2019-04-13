1c9f459cd0309a0a3b59c8f41cef7a5b56519af6
Fer <ferraniusd@gmail.com>
2019-04-13 21:08:10 +0200

    Added moving obstacle

---
+++ ProjectDuel/Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int damage = 1;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BulletDestroyer")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.tag == "CrabBlue")
+        {
+            other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
+            other.gameObject.GetComponent<Animator>().SetTrigger("CrabHit");
+            other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
+            Messenger<int>.Broadcast(GameEvent.BLUE_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
+            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+            //this.gameObject.GetComponent<Collider>().enabled = false;
+            //Destroy(this.gameObject);
+        }
+        else if (other.gameObject.tag == "CrabYellow")
+        {
+            other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
+            other.gameObject.GetComponent<Animator>().SetTrigger("CrabHit");
+            other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
+            Messenger<int>.Broadcast(GameEvent.YELLOW_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
+            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+            //this.gameObject.GetComponent<Collider>().enabled = false;
+            //Destroy(this.gameObject);
+        }
+        else if (other.gameObject.tag == "ShootYellow" || other.gameObject.tag == "ShootBlue" || other.gameObject.tag == "AcidBullet")
+        {
+            Destroy(other.gameObject);
+            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+            StartCoroutine("EnemyDeath");
+        }
+    }
+
+    IEnumerator EnemyDeath()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Destroy(this.gameObject);
+    }
+}
+
---
+++ ProjectDuel/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+    private List<GameObject> spawnPositions;
+    private bool spawnNext;
+    public string exitTowards; // right or left
+    public int minSeconds;
+    public int maxSeconds;
+    void Start()
+    {
+        minSeconds = 5;
+        maxSeconds = 10;
+        spawnNext = true;
+        spawnPositions = new List<GameObject>();
+        foreach (Transform child in this.gameObject.transform)
+        {
+            spawnPositions.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnNext) StartCoroutine("SpawnEnemy");
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        if (exitTowards == "left")
+        {
+            spawnNext = false;
+            int randomSeconds = Random.Range(minSeconds, maxSeconds);
+            yield return new WaitForSeconds(randomSeconds);
+            int randomPosition = Random.Range(0, 5);
+            Instantiate(enemy, spawnPositions[randomPosition].transform.position, enemy.transform.rotation, gameObject.transform);
+            spawnNext = true;
+        }
+        else if (exitTowards == "right")
+        {
+            spawnNext = false;  
+            int randomSeconds = Random.Range(minSeconds, maxSeconds);
+            yield return new WaitForSeconds(randomSeconds);
+            int randomPosition = Random.Range(0, 5);
+            Instantiate(enemy, spawnPositions[randomPosition].transform.position, Quaternion.Euler(0, -90, 0), gameObject.transform);
+            spawnNext = true;
+        }
+
+    }
+}
