e83959d67ad0a7198be72e854612a6e41e571a65
Saúl <saul_ibz11@hotmail.com>
2019-04-21 14:14:07 +0200

    Tiburón rompe planta amarilla

--- ProjectDuel/Assets/Scripts/Enemy.cs
+++ ProjectDuel/Assets/Scripts/Enemy.cs
@@ -9,43 +9,46 @@ public class Enemy : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "BulletDestroyer")
+        bool haveShield;
+        switch(other.gameObject.tag)
         {
-            Destroy(this.gameObject);
-        }
-        else if (other.gameObject.tag == "CrabBlue")
-        {
-            bool haveShield = other.gameObject.GetComponent<PlayerCharacter>().haveShieldPU();
-            other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
-            if (!haveShield)
-            {
-                other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
-                Debug.Log("Me queda de vida: " + other.gameObject.GetComponent<PlayerCharacter>().get_health());
-                Messenger<int>.Broadcast(GameEvent.BLUE_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
-            }
-            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
-            //this.gameObject.GetComponent<Collider>().enabled = false;
-            //Destroy(this.gameObject);
-        }
-        else if (other.gameObject.tag == "CrabYellow")
-        {
-            bool haveShield = other.gameObject.GetComponent<PlayerCharacter>().haveShieldPU();
-            other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
-            if (!haveShield)
-            {
-                other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
-                Debug.Log("Me queda de vida: " + other.gameObject.GetComponent<PlayerCharacter>().get_health());
-                Messenger<int>.Broadcast(GameEvent.YELLOW_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
-            }
-            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
-            //this.gameObject.GetComponent<Collider>().enabled = false;
-            //Destroy(this.gameObject);
-        }
-        else if (other.gameObject.tag == "ShootYellow" || other.gameObject.tag == "ShootBlue" || other.gameObject.tag == "AcidBullet")
-        {
-            Destroy(other.gameObject);
-            this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
-            StartCoroutine("EnemyDeath");
+            case "BulletDestroyer":
+            case "BulletRedirectioner":
+                Destroy(other.gameObject);
+                break;
+            case "CrabBlue":
+                haveShield = other.gameObject.GetComponent<PlayerCharacter>().haveShieldPU();
+                other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
+                if (!haveShield)
+                {
+                    other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
+                    Debug.Log("Me queda de vida: " + other.gameObject.GetComponent<PlayerCharacter>().get_health());
+                    Messenger<int>.Broadcast(GameEvent.BLUE_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
+                }
+                this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+                //this.gameObject.GetComponent<Collider>().enabled = false;
+                //Destroy(this.gameObject);
+                break;
+            case "CrabYellow":
+                haveShield = other.gameObject.GetComponent<PlayerCharacter>().haveShieldPU();
+                other.gameObject.GetComponent<PlayerCharacter>().Hurt(damage);
+                if (!haveShield)
+                {
+                    other.gameObject.GetComponent<PlayerInput>().setBeingHit(true);
+                    Debug.Log("Me queda de vida: " + other.gameObject.GetComponent<PlayerCharacter>().get_health());
+                    Messenger<int>.Broadcast(GameEvent.YELLOW_HURT, other.gameObject.GetComponent<PlayerCharacter>().get_health());
+                }
+                this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+                //this.gameObject.GetComponent<Collider>().enabled = false;
+                //Destroy(this.gameObject);
+                break;
+            case "ShootYellow":
+            case "ShootBlue":
+            case "AcidBullet":
+                Destroy(other.gameObject);
+                this.gameObject.GetComponent<Animator>().SetTrigger("enemyHit");
+                StartCoroutine("EnemyDeath");
+                break;
         }
     }
 
