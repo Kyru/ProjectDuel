9a5cb74290c696e23755e0f444156d4ff6f9ee8d
fer <ferraniusd@gmail.com>
2019-04-21 13:46:15 +0200

    Added menu sounds

---
+++ ProjectDuel/Assets/Scripts/AudioPlayClip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayClip : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    public int clipIndex;
+    [SerializeField] private AudioSource audioSourceMain;
+    [SerializeField] private AudioSource audioSourceSoundFX;
+
+    private void Start()
+    {
+        PlayAudioClip(clipIndex);
+    }
+
+    public void PlayAudioClip(int clipToPlay)
+    {
+        if (clipToPlay == 0)
+        {
+            audioSourceMain.clip = audioClips[clipToPlay];
+            audioSourceMain.Play();
+        }
+        else
+        {
+            audioSourceSoundFX.clip = audioClips[clipToPlay];
+            audioSourceSoundFX.Play();
+        }
+    }
+}
